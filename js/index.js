d1cf8d7d67ac68cd51ae2ecab4dabbfc2b02e69c
SpaceVasyl <109691084+SpaceVasyl@users.noreply.github.com>
2023-10-26 22:36:42 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,22 @@
-const book = {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    genres: ["historical prose", "adventure"],
-    isPublic: true,
-    rating: 8.38,
-  };
-  
-const tom = Object.create(book);
-tom.pages = 643;
-for (const key in tom) {
-   console.log(key);
+const arrayOfObjects = [
+  { name: "John", age: 30, city: "New York" },
+  { name: "Alice", age: 25, city: "Los Angeles" },
+  { name: "Bob", age: 35, city: "Chicago" },
+  { name: "Eva", age: 28, city: "San Francisco" },
+];
+//перше завдання
+// const nameAndAgeOfEachPerson = function (arrayOf) {
+//   for (const arr of arrayOf){
+//     console.log(`${arr.name} is ${arr.age} years old`);
+//   }
+// }
+// nameAndAgeOfEachPerson(arrayOfObjects);
+//друге завдання
+const mediumAge = function (ageOfPersons) {
+let ages = null;
+for (const age of ageOfPersons){
+  ages.push(age.age)
 }
+console.log(ages);
+}
+mediumAge(arrayOfObjects);
